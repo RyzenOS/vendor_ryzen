1e65769ccb2d224ee89d1c113c5347f1ec2bc1b4
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2023-02-05 13:57:02 +0000

    Import Bootanimation from Pixel
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,3 +1,15 @@
-# for now we are shipping default res , might add more res later
+# Boot Animation
 
-PRODUCT_COPY_FILES += vendor/ryzen/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# 720p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/ryzen/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# 1080p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+PRODUCT_COPY_FILES += vendor/ryzen/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# 1440p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/ryzen/prebuilt/common/bootanimation/1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# Default to 1080p if the device does not set the flag.
+else
+PRODUCT_COPY_FILES += vendor/ryzen/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
